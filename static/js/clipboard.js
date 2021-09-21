9fab83fa6e7fe9df5cea9c28dbdc136e09ce6277
Manak Mishra <42020940+manakmishra@users.noreply.github.com>
2021-09-21 12:17:59 +0530

    modified cliboard script, added href to logo

--- static/js/clipboard.js
+++ static/js/clipboard.js
@@ -1,7 +1,14 @@
-function myFunction() {
-    var copyText = document.getElementById("short_url");
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value);
-    alert("Copied the URL: " + copyText.value);
-  }
+function copyToClipboard() {
+    var element = document.getElementById("short_url");
+    element.select();
+    element.setSelectionRange(0, 99999);
+    
+    navigator.clipboard.writeText(element.value).then(function() {
+      //Successfullt copied
+    }, function() {
+      //fallback using deprecated execCommand API
+      document.execCommand("copy");
+    });
+
+    alert("Copied to Clipboard!");
+}
